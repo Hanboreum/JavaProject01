bfd4720705bba46fb623fffbb6912fd89f2f4172
fullm <matoma1_@naver.com>
2024-03-18 11:30:50 +0900

    20240318
    고객 등록 삭제 검색 Controller

---
+++ src/main/java/kr/pet/mvc/CustomerController.java
@@ -0,0 +1,11 @@
+package kr.pet.mvc;
+
+import java.util.List;
+
+public class CustomerController {
+    //고객 등록 삭제 검색
+    private List<Customer> customers;
+    private MedicalRecordController recordController; //진료기록관리 컨트롤러
+
+
+}
